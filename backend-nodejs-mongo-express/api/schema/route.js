96755d7ac732e276f840601b0ec3acc1b610ebca
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-14 13:24:23 +0500

    update schema by adding location

--- backend-nodejs-mongo-express/api/schema/route.js
+++ backend-nodejs-mongo-express/api/schema/route.js
@@ -1,17 +1,29 @@
 const mongoose = require('mongoose')
 
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: "Point"
+    },
+    coordinates = {
+        type: [Number],
+        index: '2dsphere'
+    }
+})
 
 const ninjaSchema = mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Name field is required']
-    },
+    }, 
     rank: {
         type: String
     },
     available: {
         type:Boolean,
         default: false
-    }
+    },
+    geometry: GeoSchema 
+    
 });
 module.exports = mongoose.model('Ninja', ninjaSchema)
