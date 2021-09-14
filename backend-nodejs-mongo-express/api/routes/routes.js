49fdbf528ad70f5722cb6fec0ab3d3b9affd4148
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-14 07:39:33 +0500

    Done with put and delete route

--- backend-nodejs-mongo-express/api/routes/routes.js
+++ backend-nodejs-mongo-express/api/routes/routes.js
@@ -29,28 +29,66 @@ router.post('/ninjas', (req,res, next)=>{
                     rank: result.rank,
                     available: result.available,
                     id: result._id,
-                    response: {
+                    request: {
                         type: "GET",
                         url: 'http://localhost:3000/api/ninjas/' + result._id
                     }
                 }
             })
-        }).catch(err=>{
-            console.log(err)
+        })
+        // .catch(next)
+        //OR
+        .catch(err=>{
+            console.log(err);
             res.status(500).json({
                 error: err
             })
         })
 })
-router.put('/ninjas/:id', (req,res, next)=>{
-    res.status(201).json({
-        type:'PUT'
-    })
+
+router.put('/ninjas/:ninjaId', (req,res, next)=>{
+    const id = req.params.ninjaId
+    Ninja.findByIdAndUpdate({_id:id}, req.body, {new:true})//always remain like this
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                name: result.name,
+                rank: result.rank,
+                available: result.available,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/api/ninjas' + result._id
+                }
+            })
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        }) 
 })
-router.delete('/ninjas:id', (req,res, next)=>{
-    res.status(201).json({
-        type:'DELETE'
-    })
+
+router.delete('/ninjas/:ninjaId', (req,res, next)=>{
+    const id = req.params.ninjaId
+    Ninja.remove({_id:id})
+        .exec()
+        .then(result=>{
+            res.status(200).json({
+                message: "Ninja deleted",
+                request:{
+                    type: 'POST',
+                    url: 'http://localhost:3000/api/ninjas',
+                    body: { name: "String", rank: 'String', available: "Boolean"}
+                }
+            })
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            })
+        })
 })
 
 module.exports = router
